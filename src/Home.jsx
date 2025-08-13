c87d1e4d2344475a9ae9d1f8fae713c5eacfc9bb
besher94 <besheraboshaaer@gmail.com>
2025-08-13 17:49:24 +0300

    g

--- src/Home.jsx
+++ src/Home.jsx
@@ -43,53 +43,74 @@ const Home = () => {
         />
       </div>
 
-      <div style={{ direction: "rtl", padding: "20px" }}>
-        {sections.map((section) => (
-          <div key={section.id} style={{ marginBottom: "40px" }}>
-            <h1>{section.title}</h1>
-            <Swiper
-              spaceBetween={15}
-              slidesPerView={2}
-              breakpoints={{
-                768: { slidesPerView: 4 },
-              }}
-            >
-              {section.products.map((product) => (
-                <SwiperSlide key={product.id}>
-                  <div
-                    id="pp"
-                    className="product-card"
-                    style={{
-                      borderRadius: "8px",
-                      padding: "10px",
-                      textAlign: "center",
-                      cursor: "pointer",
-                    }}
-                    onClick={() => navigate(`/product/${product.id}`)}
-                  >
-                    <img
-                      src={product.image}
-                      alt={product.name}
+      <div
+        className="main-flex"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div style={{ padding: "20px", width: "80%" }}>
+          {sections.map((section) => (
+            <div key={section.id} style={{ marginBottom: "40px" }}>
+              <h1>{section.title}</h1>
+              <Swiper
+                spaceBetween={15}
+                slidesPerView={2}
+                breakpoints={{
+                  768: { slidesPerView: 4 },
+                }}
+              >
+                {section.products.map((product) => (
+                  <SwiperSlide key={product.id}>
+                    <div
+                      id="pp"
+                      className="product-card"
                       style={{
-                        width: "100%",
-                        height: "150px",
-                        objectFit: "cover",
-                        borderRadius: "5px",
+                        borderRadius: "8px",
+                        padding: "10px",
+                        textAlign: "center",
+                        cursor: "pointer",
                       }}
-                    />
-                    <h2 style={{ fontSize: "24px", margin: "10px 0 5px" }}>
-                      {product.name}
-                    </h2>
-                    <h2 style={{ fontSize: "18px", color: "#555" }}>
-                      {product.description}
-                    </h2>
-                    <h2 id="product-price">{product.price} $</h2>
-                  </div>
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
-        ))}
+                      onClick={() => navigate(`/product/${product.id}`)}
+                    >
+                      <img
+                        src={product.image}
+                        alt={product.name}
+                        style={{
+                          width: "100%",
+                          height: "150px",
+                          objectFit: "cover",
+                          borderRadius: "5px",
+                        }}
+                      />
+                      <h2
+                        style={{
+                          fontSize: "24px",
+                          margin: "10px 0 5px",
+                          fontWeight: "500",
+                        }}
+                      >
+                        {product.name}
+                      </h2>
+                      <h2
+                        style={{
+                          fontSize: "18px",
+                          color: "#555",
+                          fontWeight: "500",
+                        }}
+                      >
+                        {product.description}
+                      </h2>
+                      <h2 id="product-price">{product.price} $</h2>
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
